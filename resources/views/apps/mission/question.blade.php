de89a43cc5480ac6a9a93649354d539dc8597f68
Sasenna <lintangpanggalih@gmail.com>
2024-01-20 18:46:12 +0700

    add grid question layout

--- resources/views/apps/mission/question.blade.php
+++ resources/views/apps/mission/question.blade.php
@@ -13,6 +13,10 @@
             width: 100%;
         }
 
+        .answer-content.cube {
+            top: 10vh;
+        }
+
         .question-board {
             color: #fff;
         }
@@ -49,12 +53,13 @@
         }
 
         @media screen and (max-width: 425px) {
-            
+
             .question-board-img {
                 top: 50vh;
                 width: 95vw;
                 height: auto;
             }
+
             div.question-board {
                 top: 26vh;
                 width: 70vw;
@@ -63,12 +68,14 @@
                 font-size: 15pt;
             }
         }
+
         @media screen and (max-width: 415px) {
             .question-board-img {
                 top: 40vh;
                 width: 95vw;
                 height: auto;
             }
+
             div.question-board {
                 top: 25%;
                 /* width: 64%; */
@@ -94,14 +101,14 @@
 
             .question-board-img {
                 top: 48%;
-                width: 60vh;
+                width: 30vw;
                 height: auto;
             }
 
             div.question-board {
-                top: 25vh;
-                width: 45vh;
-                height: 100px;
+                top: 30%;
+                width: 20vw;
+                height: auto;
                 /* font-weight: bold; */
                 font-size: 18pt;
             }
@@ -113,24 +120,41 @@
                 color: maroon;
                 font-size: 15pt;
             }
+
+            .answer-content.cube {
+                top: 20vh;
+            }
         }
     </style>
     <img class="centered-img question-board-img" src="{{ asset('img/elements/28.png') }}" alt="">
     <div class="container" style="height: 60vh;margin-top: 40vh;">
-        <div class="centered-img question-board" style="border: red 1pt solid;">
+        <div class="centered-img question-board">
             {{ $quiz->question }}
         </div>
         <div class="row" id="answers" style="text-align: center;">
-            @foreach ($quiz->answers as $answer)
-                <div class="col-12">
-                    <button class="btn-submit-img">
-                        <img src="{{ asset('img/elements/29-copy.png') }}" alt="" height="110vh" width="100%">
-                        <div class="centered-img answer-content">
-                            {{ $answer->option }}
-                        </div>
-                    </button>
-                </div>
-            @endforeach
+            @if ($quiz->order % 2 == 0)
+                @foreach ($quiz->answers as $answer)
+                    <div class="col-6 p-0 m-0">
+                        <button class="btn-submit-img">
+                            <img src="{{ asset('img/elements/30.png') }}" alt="" height="auto" width="100%">
+                            <div class="centered-img answer-content cube" style="width: 70%;">
+                                {{ $answer->option }}
+                            </div>
+                        </button>
+                    </div>
+                @endforeach
+            @else
+                @foreach ($quiz->answers as $answer)
+                    <div class="col-12">
+                        <button class="btn-submit-img">
+                            <img src="{{ asset('img/elements/29-copy.png') }}" alt="" height="110vh" width="100%">
+                            <div class="centered-img answer-content">
+                                {{ $answer->option }}
+                            </div>
+                        </button>
+                    </div>
+                @endforeach
+            @endif
         </div>
     </div>
 @endsection
